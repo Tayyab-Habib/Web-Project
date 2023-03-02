06aa6da12950eea1b0015f26b939f57c178f2873
Tayyab-Habib <tayyabhabib34567@gmail.com>
2023-03-02 11:17:59 +0500

    Freelancing website

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
+
